65e8858865041132efd1d799dfbfa00926fdd3fb
AfeefM <65280987+AfeefM@users.noreply.github.com>
2020-05-16 20:08:53 +0530

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,4 +1,4 @@
-#write a program to insert an element into an array without using any inbuilt functions
+#write a program to insert an element into an array to the n th position without using any inbuilt functions
 arr=[1,2,3,4,5,6,7,8,9,10]   #array
 position=3       #position to which element to be inserted
 insert_num=5     #element to be inserted
