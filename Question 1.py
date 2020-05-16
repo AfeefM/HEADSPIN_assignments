61410205345788d43805320a6e4b48074995802d
AfeefM <65280987+AfeefM@users.noreply.github.com>
2020-05-16 19:27:56 +0530

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,8 +1,9 @@
-ar=[1,2,3,4,5,6,7,8,9,10]
-position=3
-insert_num=5
-index=position-1
+#write a program to insert an element into an array without using any inbuilt functions
+arr=[1,2,3,4,5,6,7,8,9,10]   #array
+position=3       #position to which element to be inserted
+insert_num=5     #element to be inserted
+index=position-1    
 ar1=arr[:index]
 ar2=[insert_num]
-ar3=[index:]
+ar3=arr[index:]
 print(ar1+ar2+ar3)
