b1b9f2460c5becc567a92eeab657a92e164588eb
AfeefM <65280987+AfeefM@users.noreply.github.com>
2020-05-16 20:45:57 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -1,6 +1,6 @@
 #write a program to display prime numbers between 2 intervals without using any inbuilt functons
-interval1=int(input("Enter lower prime number"))  
-interval2=int(input("Enter higher prime number"))  
+interval1=int(input("Enter lower prime number"))  #lower interwal
+interval2=int(input("Enter higher prime number"))  #higer interval
 for num in range(interval1,interval2 + 1): 
    if num > 1:  
        for i in range(2,num):  
